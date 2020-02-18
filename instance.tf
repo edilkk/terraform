ecd6083f3471f9e34d034b12c8a279b85a988121
edilkk <edilmoldokanov@gmail.com>
2020-02-18 11:44:18 -0600

    SDAS

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "web1" {
   ami = "ami-09022592b5cce287f"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.terraform_april.key_name}"
-  security_groups = ["${aws_security_group.sec1.id}"]
+  security_groups = ["${aws_security_group.sec1.name}"]
 
   tags = {
       Name = "Dev"
