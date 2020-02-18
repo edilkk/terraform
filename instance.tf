9586da5e26b04b8993404f91fe3f36ac047d3b92
edilkk <edilmoldokanov@gmail.com>
2020-02-18 11:36:29 -0600

    tgs added

--- instance.tf
+++ instance.tf
@@ -3,4 +3,11 @@ resource "aws_instance" "web1" {
   ami = "ami-09022592b5cce287f"
   instance_type = "t2.micro"
   key_name = "terraform_april"
+
+  tags = {
+      Name = "Dev"
+      Dept = "IT"
+      Group = "April"
+      Created_by = "Ed"
+  }
 }
