6633333b5a8907d5591e2c97b013ca4f67125c1d
edilkk <edilmoldokanov@gmail.com>
2020-02-18 11:22:44 -0600

    added sec grp

---
+++ sec_group.tf
@@ -0,0 +1,12 @@
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow SSH"
+  vpc_id      = "vpc-2318d848"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "-1"
+    cidr_blocks = "0.0.0.0/0"
+  }
+}
