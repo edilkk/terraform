06af4fa2f590bca8e8b8d5775154bbdac4f8d62b
edilkk <edilmoldokanov@gmail.com>
2020-02-18 11:41:49 -0600

    add

--- instance.tf
+++ instance.tf
@@ -2,7 +2,8 @@
 resource "aws_instance" "web1" {
   ami = "ami-09022592b5cce287f"
   instance_type = "t2.micro"
-  key_name = "terraform_april"
+  key_name = "${aws_key_pair.terraform_april.key_name}"
+  security_groups = ["${aws_security_group.sec1.id}"]
 
   tags = {
       Name = "Dev"
